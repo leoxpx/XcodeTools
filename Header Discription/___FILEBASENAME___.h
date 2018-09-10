b646e3f88b73124fb607ff3a5de22a7503e2451e
xupengxiang <xupengxiang@xupengxiangdeMac-mini.local>
2018-09-10 18:46:36 +0800

    first

---
+++ Header Discription/___FILEBASENAME___.h
@@ -0,0 +1,18 @@
+/*******************************************************************************************************
+ *
+ * @FileName:     ___FILENAME___
+ * @ProjectName:  ___PROJECTNAME___
+ * @CreateDate:   Created by ___FULLUSERNAME___ on ___DATE___.
+ * @Copyright:    ___COPYRIGHT___
+ *
+ * @Discription:  描述
+ *
+ ******************************************************************************************************/
+
+
+___IMPORTHEADER_cocoaTouchSubclass___
+
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_cocoaTouchSubclass___
+
+@end
---
+++ Header Discription/___FILEBASENAME___.m
@@ -0,0 +1,18 @@
+/*******************************************************************************************************
+ *
+ * @FileName:     ___FILENAME___
+ * @ProjectName:  ___PROJECTNAME___
+ * @CreateDate:   Created by ___FULLUSERNAME___ on ___DATE___.
+ * @Copyright:    ___COPYRIGHT___
+ *
+ * @Discription: 
+ *
+ ******************************************************************************************************/
+
+
+#import "___FILEBASENAME___.h"
+
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+@end
